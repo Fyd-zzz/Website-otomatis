13e53352665fcb359d59612e08815b60b8e08208
Fydzzz <svnoprib@gmail.com>
2026-03-16 14:42:47 +0800

    Enhance IP display with remote and public IPs
    Added additional IP information display including remote IP, hostname, and public IP.

--- ip.php
+++ ip.php
@@ -1,3 +1,7 @@
 <?php
-echo $_SERVER['SERVER_ADDR'];
+echo "IP Server: " . $_SERVER['SERVER_ADDR'] . "<br>";
+echo "IP Remote: " . $_SERVER['REMOTE_ADDR'] . "<br>";
+echo "Host: " . gethostname() . "<br>";
+$ip = file_get_contents('https://api.ipify.org');
+echo "Public IP: " . $ip;
 ?>
